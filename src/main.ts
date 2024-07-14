1436333c0cd41181068443fa410217c86dd66e29
Tim Stanton <thstanton@proton.me>
2024-07-14 15:21:47 +0100

    Prisma client exception filter added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalFilters(new PrismaClientExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
