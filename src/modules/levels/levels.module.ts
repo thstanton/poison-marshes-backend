3d80a00cf68a6a50fb51f6ace4dc3c87cd14763d
Tim Stanton <thstanton@proton.me>
2024-07-30 16:22:40 +0100

    Fixed missing import

--- src/modules/levels/levels.module.ts
+++ src/modules/levels/levels.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LevelsService } from './levels.service';
 import { LevelsRepository } from './levels.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { ResendModule } from '../resend/resend.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ResendModule],
   providers: [LevelsService, LevelsRepository],
   exports: [LevelsService],
 })
