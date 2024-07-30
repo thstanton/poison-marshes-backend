2775e10cf9831cc06ea395a1b25aa1c2ee1b4289
Tim Stanton <thstanton@proton.me>
2024-07-30 16:13:30 +0100

    Fixed missing import of AuthModule in AccountsModule

--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsRepository } from './accounts.repository';
 import { PrismaModule } from 'src/database/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   providers: [AccountsService, AccountsRepository],
   exports: [AccountsService],
 })
