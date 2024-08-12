416cb461f548ce43570c66ca0d2f1a370f9b066f
Tim Stanton <thstanton@proton.me>
2024-08-12 15:15:21 +0100

    Simplified CORS setup

--- src/main.ts
+++ src/main.ts
@@ -2,34 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './errors/prisma-client-exception/prisma-client-exception.filter';
 import * as cookieParser from 'cookie-parser';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  let corsOptions: CorsOptions = {};
-  if (process.env.NODE_ENV === 'development') {
-    const allowedOrigins = [/\.vercel\.app$/];
-
-    corsOptions = {
-      origin: function (origin, callback) {
-        if (allowedOrigins.some((pattern) => pattern.test(origin))) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      credentials: true,
-    };
-  } else {
-    corsOptions = {
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    };
-  }
-
-  app.enableCors(corsOptions);
-
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  });
   app.use(cookieParser(process.env.COOKIE_SECRET));
   app.useGlobalFilters(new PrismaClientExceptionFilter());
   await app.listen(3000);
